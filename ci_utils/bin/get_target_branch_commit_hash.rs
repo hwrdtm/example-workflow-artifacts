ad6e42b7c217913253cce1884bddad2963f9a641
Howard Tam <howard.tam.95@gmail.com>
2024-03-22 11:02:34 -0700

    Implement get_target_branch_commit_hash

---
+++ ci_utils/bin/get_target_branch_commit_hash.rs
@@ -0,0 +1,12 @@
+use ci_utils::get_target_branch_commit_hash;
+
+fn main() {
+    // Get the name of the target branch from the 1st command line argument.
+    let target_branch = std::env::args().nth(1).expect("No target branch provided");
+
+    let target_branch_commit_hash = get_target_branch_commit_hash(&target_branch)
+        .expect("Failed to get target branch commit hash");
+
+    // Print the target branch commit hash.
+    println!("{}", target_branch_commit_hash);
+}
