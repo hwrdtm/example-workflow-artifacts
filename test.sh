24dbe949587bf72125698bce9e8620f1ec5d8e91
Howard Tam <howard.tam.95@gmail.com>
2024-03-25 16:06:58 -0700

    Debug workflow

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 git --version
 git config --global pager.branch false
 
+git branch --remotes
+
 # Run git branch -al main and trim the first character
 git branch -al main | cut -c 3-
 git branch -al release-1
